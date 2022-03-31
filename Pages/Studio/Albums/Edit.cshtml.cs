80fd028b539818de3a43bd9502b043e64f2dd5f6
JasmaineWilliams <98206198+JasmaineWilliams@users.noreply.github.com>
2022-03-31 02:45:03 -0500

    Studio Album Changes
    Completed the studio album index page and the create album page.

---
+++ Pages/Studio/Albums/Edit.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MusicLibrary.Pages.Studio.Albums
+{
+	public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
